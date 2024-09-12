28edb46b9bbce7a2015f40835abaf9a22da42bfc
An <tango.11@yahoo.com>
2024-09-12 10:54:22 +0700

    update cpu

--- main.cpp
+++ main.cpp
@@ -7,28 +7,26 @@
 using namespace std;
 
 double getCpuUsage() {
-    long prevIdleTime = 0, prevTotalTime = 0;
-    long user, nice, sys, idle, iowait, irq, softirq, cpu, steal;
+    static long prevIdleTime = 0, prevTotalTime = 0;
+    long user, nice, sys, idle, iowait, irq, softirq, steal;
+    string cpu;
 
     ifstream fp("/proc/stat");
     string line;
     getline(fp, line);
-    // fp.close();
+    fp.close();
 
     istringstream iss(line);
     iss >> cpu >> user >> nice >> sys >> idle >> iowait >> irq >> softirq >> steal;
 
     long idleTime = idle + iowait;
-    long total = user + nice + sys + idle + irq + softirq;
+    long total = user + nice + sys + idle + irq + softirq + steal;
 
     double usage = 100.0 * (1.0 - (double)(idleTime - prevIdleTime) / (total - prevTotalTime));
 
-    prevIdleTime = idle;
+    prevIdleTime = idleTime;
     prevTotalTime = total;
 
-        cout << prevIdleTime << endl;
-        cout << idle << endl;
-        cout << iowait << endl;
     return usage;
 }
 
@@ -39,10 +37,10 @@ int main() {
     // }
     //
     while (true) {
-        cout << getCpuUsage() << endl;
-        cout << "hello" << endl;
+        cout << "Your CPU" << endl;
+        cout << getCpuUsage() << '%' << endl;
         this_thread::sleep_for(chrono::seconds(1));
-        // system("clear");
+        system("clear");
     }
     return 0;
 }
